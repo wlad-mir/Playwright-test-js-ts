10893d3e455a68887d4ca5f838d5d0e5fb30e014
wlad-mir <qaengineer@int.pl>
2026-04-20 21:41:02 +0200

    Separate data and logic

--- tests/mainPage.spec.ts
+++ tests/mainPage.spec.ts
@@ -1,54 +1,20 @@
 import { test, expect } from '@playwright/test';
-import { headerElements, headerElementNames, headerHrefs } from '../locators/header.locators';
+import { HeaderPage } from '../pages/header.page';
 
 test.describe('Home Page Tests', () => {
   test.beforeEach(async ({ page }) => {
     await page.goto('https://playwright.dev/');
   });
 
-  // === TEST 1: Checking the visibility of elements ===
-  test('Checking the display of navigation elements header', async ({ page }) => {
-    for (const getLocator of headerElements) {
-      await expect.soft(getLocator(page)).toBeVisible();
-    }
+  test('Header tests using Page Object', async ({ page }) => {
+    const header = new HeaderPage(page);
+
+    await header.expectAllVisible();
+    await header.expectTitles();
+    await header.expectHrefs();
+    await header.toggleTheme();
   });
 
-  // === TEST 2: Checking the text of elements ===
-  test('Checking the title of navigation elements header', async ({ page }) => {
-    for (let i = 0; i < headerElementNames.length; i++) {
-      if (headerElementNames[i] !== null) {
-        await expect.soft(headerElements[i](page)).toContainText(headerElementNames[i]!);
-      }
-    }
-  });
-
-  // === TEST 3: Checking href elements ===
-  test('Checking the href attribute of header navigation elements', async ({ page }) => {
-    for (let i = 0; i < headerHrefs.length; i++) {
-      if (headerHrefs[i] !== null) {
-        await expect.soft(headerElements[i](page)).toHaveAttribute('href', headerHrefs[i]!);
-      }
-    }
-  });
-
-  // === TEST 4: Checking topic switching ===
-  test('Theme switch updates data-theme-choice and localStorage', async ({ page }) => {
-    const themeButton = headerElements[8](page);
-    const html = page.locator('html');
-
-    const initialChoice = await html.getAttribute('data-theme-choice');
-    await themeButton.click();
-
-    const expectedChoice =
-      initialChoice === 'light' ? 'dark' : initialChoice === 'dark' ? 'light' : 'light';
-
-    await expect.soft(html).toHaveAttribute('data-theme-choice', expectedChoice);
-
-    const storedTheme = await page.evaluate(() => localStorage.getItem('theme'));
-    expect(storedTheme).toBe(expectedChoice);
-  });
-
-  // === TEST 5: Checking the page title ===
   test('Checking the page title', async ({ page }) => {
     const heading = page.getByRole('heading', { name: 'Playwright enables reliable' });
 
@@ -60,7 +26,6 @@ test.describe('Home Page Tests', () => {
       );
   });
 
-  // === TEST 6: Testing the “Get Started” button ===
   test('Testing the Get Started button', async ({ page }) => {
     const btn = page.getByRole('link', { name: 'Get started' });
 
